e7d153360e536d1b4f281b8f8568363770e44c31
Joshmatjjen <joshmatparrot@gmail.com>
2021-05-27 03:05:45 +0100

    first commit

---
+++ src/pages/about/index.js
@@ -0,0 +1,14 @@
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>A JavaScript programmer</p>
+    <img
+      src="/static/andela.jpg"
+      alt="Javascript"
+      width="200px"
+      height="200px"
+    />
+  </div>
+);
+
+export default About;
